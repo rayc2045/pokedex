9c7bd6e29acc0473f44add1e8b97c67043bf88a9
rayc2045 <>
2023-01-06 19:17:30 +0800

    Update api

--- scripts/script.js
+++ scripts/script.js
@@ -25,26 +25,33 @@ const App = {
     return `${(this.pokemons.length / this.max) * 100}%`;
   },
   async init() {
-    const promises = [];
-    for (let i = 1; i <= this.max; i++) promises.push(this.fetchPokemon(i));
-    await Promise.all(promises);
-    this.pokemons.sort((a, b) => a.id - b.id);
+    const pokemons = await fetch('../data/pokeapi.json')
+      .then(res => res.json());
+    this.pokemons = pokemons;
+    pokemons.forEach(pokemon =>
+      pokemon.types.forEach(type => {
+        if (!this.types.includes(type)) this.types.push(type);
+    }));
+    // const promises = [];
+    // for (let i = 1; i <= this.max; i++) promises.push(this.fetchPokemon(i));
+    // await Promise.all(promises);
+    // this.pokemons.sort((a, b) => a.id - b.id);
   },
-  async fetchPokemon(id) {
-    const pokemon = await util.fetchData(
-      `https://pokeapi.co/api/v2/pokemon/${id}`
-    );
-    const types = pokemon.types.map(type => type.type.name);
-    this.pokemons.push({
-      id,
-      name: pokemon.name,
-      image: pokemon.sprites.other['official-artwork']['front_default'], // pokemon.sprites['front_default'],
-      types,
-    });
-    types.forEach(type => {
-      if (!this.types.includes(type)) this.types.push(type);
-    });
-  },
+  // async fetchPokemon(id) {
+  //   const pokemon = await util.fetchData(
+  //     `https://pokeapi.co/api/v2/pokemon/${id}`
+  //   );
+  //   const types = pokemon.types.map(type => type.type.name);
+  //   this.pokemons.push({
+  //     id,
+  //     name: pokemon.name,
+  //     image: pokemon.sprites.other['official-artwork']['front_default'], // pokemon.sprites['front_default'],
+  //     types,
+  //   });
+  //   types.forEach(type => {
+  //     if (!this.types.includes(type)) this.types.push(type);
+  //   });
+  // },
 };
 
 createApp(App).mount();
